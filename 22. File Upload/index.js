65fb87dd669b6b19d25917c03df9179f16d23f9a
ratul43 <kmratul971@gmail.com>
2024-11-02 17:23:13 +0600

    File Upload

---
+++ 22. File Upload/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+
+ var multer = require('multer');
+ var app = express();
+
+
+ var storage = multer.diskStorage({
+     destination: function (req, file, callBack){
+         callBack(null, './uploads');
+     },
+
+     filename: function (req, file, callBack){
+         callBack(null,file.originalname);
+     }
+ })
+
+
+
+    var upload = multer({storage: storage}).single('myfile')
+
+
+app.post('/', function (req, res) {
+    upload(req,res,function(error){
+        if(error){
+            res.send("File upload fail")
+        }
+        else{
+            res.send("File upload success")
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+app.listen(8000, function(){
+    console.log("Server run success");
+})
+
+
